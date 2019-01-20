daab0f194fccf41d0d8670fccc6fc6b6ff015094
n.f <ec2-user@ip-172-31-13-71.ec2.internal>
2019-01-20 14:18:27 +0000

    modify tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,14 +44,14 @@ class TasksController < ApplicationController
     flash[:success] = "タスクを削除しました"
     redirect_to tasks_url
   end
-end
+  
+  private 
 
-private 
-
-def set_task
-  @task = Task.find(params[:id])
-end
-
-def task_params
-  params.require(:task).permit(:content, :status)
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
